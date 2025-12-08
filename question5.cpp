1d1ee87e6d1d7c498e662a8278ce9d971d21544e
rajputanjali <rajputanjali7211@gmail.com>
2025-12-08 20:17:59 +0530

    question 5

---
+++ question5.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 7, b = 8;
+    int product = a * b;
+    cout << "the product of 7 and 8 is:" << product;
+    return 0;
+}
