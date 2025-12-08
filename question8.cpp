e74452f248ae9add3230530af9dd4a9dd534b0d2
rajputanjali <rajputanjali7211@gmail.com>
2025-12-08 20:45:29 +0530

    question 8

---
+++ question8.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num1, num2, sum;
+
+    cout << "enter first numbar:";
+    cin >> num1;
+
+    cout << "enter the number:";
+    cin >> num2;
+
+    sum = num1 + num2;
+    cout << "the sum of " << num1 << " and " << num2 << " is " << sum;
+    return 0;
+}
