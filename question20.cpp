abe6d4d87c876046345ee38bcc0629a2cde4f1d2
rajputanjali <rajputanjali7211@gmail.com>
2025-12-10 12:15:20 +0530

    question 20

---
+++ question20.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b, c, sum;
+    cout << "enter the first number:";
+    cin >> a;
+    cout << "enter the second number:";
+    cin >> b;
+    cout << "enter the third number:";
+    cin >> c;
+    sum = a + b + c;
+    cout << "the sum of the three number is:" << sum;
+    return 0;3
+}
