8b58d9ee99d385d9b6ed802d07ac8a61f91c9573
rajputanjali <rajputanjali7211@gmail.com>
2025-12-11 12:48:51 +0530

    question 23

---
+++ question23.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int number;
+    cout << "enter the favorite number:";
+    cin >> number;
+    cout << "my favorite number is" << number;
+    return 0;
+}
