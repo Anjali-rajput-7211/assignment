44a4006ee6c9bf1b57ec4dfd1330062d95bc1944
rajputanjali <rajputanjali7211@gmail.com>
2025-12-11 12:37:26 +0530

    question 22

---
+++ question22.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a = 6, b = 9;
+    int product = a * b;
+    cout << "the product of " << a << " and " << b << " is:" << product;
+    return 0;
+}
