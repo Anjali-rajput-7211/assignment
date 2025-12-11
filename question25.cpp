0a82fa97974fc764fcc4bf4613cef43e4ffbb8ac
rajputanjali <rajputanjali7211@gmail.com>
2025-12-11 13:02:26 +0530

    question 25

---
+++ question25.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int age;
+    cout << "enter your age:";
+    cin >> age;
+    cout << "i am " << age << " year old";
+    return 0;
+}
