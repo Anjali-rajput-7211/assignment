cb160b2bc2195d6216cdc1ed4c26ed927f5d6537
rajputanjali <rajputanjali7211@gmail.com>
2025-12-12 14:42:27 +0530

    question27

---
+++ question27.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string city;
+    cout << "Enter your city :--->";
+    cin >> city;
+
+    cout << "i am living in " << city << endl;
+    return 0;
+}
