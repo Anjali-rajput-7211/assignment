085d18466dc9e92ca3adaf82f294aa8423060483
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 16:34:15 +0530

    question41

---
+++ question41.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string name, city, institute;
+
+    cout << "enter name:";
+    cin >> name;
+    cout << "enter city:";
+    cin >> city;
+    cout << "enter the institute:";
+    cin >> institute;
+    cout << "hello my self " << name << " I am from " << city << " I have completed bca from " << institute;
+    return 0;
+}
