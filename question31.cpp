9e1fb20a75ea941b7a03b6553ba5f19f1667cb9e
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 15:14:52 +0530

    question32

---
+++ question31.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string city, hobby;
+    cout << "enter the city:";
+    cin >> city;
+    cout << "enter the hobby:";
+    cin >> hobby;
+    cout << "i am from" << city << "my hobby " << hobby;
+    return 0;
+}
---
+++ question32.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    cout << "enter the number of pet:";
+    cin >> i;
+    cout << "I own " << i << " pets";
+    return 0;
+}
