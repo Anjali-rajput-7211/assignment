2f55abb4490e0f9235157242635e77873610a1ee
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 14:48:49 +0530

    question30

---
+++ question30.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string job_titel;
+    cout << "enter your job titel:";
+    cin >> job_titel;
+    cout << "i am working as an " << job_titel;
+    return 0;
+}
