b12ae4510c52204540d122a018de5a8b347f0bbf
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 17:06:37 +0530

    question44

---
+++ question44.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float n1, n2, n3, n4, n5, average;
+
+    cout << "enter the five number:";
+    cin >> n1 >> n2 >> n3 >> n4 >> n5;
+
+    average = (n1 + n2 + n3 + n4 + n5) / 5;
+    cout << "the average of the five number is:" << average << endl;
+    return 0;
+}
