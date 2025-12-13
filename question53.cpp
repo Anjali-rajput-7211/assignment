31368c8143eea5f9ca093631d95cba98447a7e5d
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 23:42:30 +0530

    question53

---
+++ question53.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char ch;
+    cout << "enter the charectar:";
+    cin >> ch;
+    cout << "the ASCII value of " << ch << " is " << int(ch) << endl;
+    return 0;
+}
