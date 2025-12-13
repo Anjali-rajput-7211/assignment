a16f0d997fd20f74b589531dcf727036c20a6374
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 15:45:24 +0530

    question35

---
+++ question35.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string city;
+    cout << "enter the city:";
+    cin >> city;
+    cout << "I plain to visit " << city << " this summer";
+    return 0;
+}
