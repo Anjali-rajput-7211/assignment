e0d39ba61a9aaf7d4807e1df58c16d01ea1e36b5
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 16:02:41 +0530

    question38

---
+++ question38.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a;
+    cout << "enter a string :";
+    cin >> a;
+    cout << "I am intrested in " << a << " as a carear";
+    return 0;
+}
