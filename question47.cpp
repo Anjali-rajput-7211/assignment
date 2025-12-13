ef0513978d28d7d8c6f0ed65ae07013beaf1c0fa
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 18:00:57 +0530

    question47

---
+++ question47.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+
+    cout << "enter the number:";
+    cin >> num;
+    if(num%2==0){
+        cout << "the number is even" << endl;
+    }
+    else
+        cout << "the number is odd" << endl;
+
+    return 0;
+}
