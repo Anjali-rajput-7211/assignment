3bb0381bec007683397a3bcc9bf4ea84df9b61f7
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 16:41:47 +0530

    question42

---
+++ question42.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b, sum;
+    cout << "enter the first number :";
+    cin >> a;
+    cout << "enter the second number :";
+    cin >> b;
+    cout << "the sum of two number :" << a + b;
+    return 0;
+}
