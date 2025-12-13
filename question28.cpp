a86f1b51ff03bd4ac171d8225226dc0526307749
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 14:29:45 +0530

    question28

---
+++ question28.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string degree, institution;
+    cout << "enter the degree:";
+    cin >> degree;
+    cout << "enter the institution";
+    cin >> institution;
+
+    cout << "i have completed " << degree << " from " << institution;
+    return 0;
+}
