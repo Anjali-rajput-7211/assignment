e0c26c69113ed9884d919db91ed2f57a90fe8e79
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 15:52:17 +0530

    question36

---
+++ question36.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a, b;
+    cout << "enter two string:";
+    cin >> a >> b;
+    cout << "I prafer " << b << " over " << a;
+    return 0;
+}
