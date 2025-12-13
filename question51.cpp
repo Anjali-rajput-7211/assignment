feaff4a40ef46ef7dc65aee748187616805184f5
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 21:29:37 +0530

    question51

---
+++ question51.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float a, b, c, product;
+
+    cout << "enter three float numbers:";
+    cin >> a >> b >> c;
+
+    product = a * b * c;
+    cout << "the product of the three number is:" << product << endl;
+    return 0;
+}
