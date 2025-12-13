4f1eeb299923266b147d48a900d53564eece2310
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 15:36:40 +0530

    question34

---
+++ question34.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cout << "enter the number :";
+    cin >> a;
+    string activity;
+    cout << "enter the activity:";
+    cin >> activity;
+    
+    cout << "I am " << a << "years old and i enjoy " << activity;
+    return 0;
+}
