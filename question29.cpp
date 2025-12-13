9935131763a614589c42a21c8ef23ba20d90f2f2
rajputanjali <rajputanjali7211@gmail.com>
2025-12-13 14:41:07 +0530

    question29

---
+++ question29.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string color;
+    cout << "enter the faveurite color:";
+    cin >> color;
+    cout << "my faveurite color is " << color;
+    return 0;
+}
