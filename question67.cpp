3db7360927907de50a45d38e38f44057fee2a64e
rajputanjali <rajputanjali7211@gmail.com>
2025-12-14 15:31:22 +0530

    question67

---
+++ question67.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter the number:";
+    cin >> n;
+    int i = 1;
+    while(i*i<=n){
+        i++;
+    }
+    cout << i - 1;
+    return 0;
+}
