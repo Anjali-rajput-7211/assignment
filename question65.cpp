baabc9bb4c7027a2be8c0c271564ba1b43c680b5
rajputanjali <rajputanjali7211@gmail.com>
2025-12-14 14:16:39 +0530

    question65

---
+++ question65.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1, s2, s3, s4, s5;
+    cout << "enter the five string";
+    cin >> s1 >> s2 >> s3 >> s4 >> s5;
+
+    cout << s1 << " " << s2 << " " << s3 << " " << s4 << " " << s5 << endl;
+
+    return 0;
+}
