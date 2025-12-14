6148d3f5a8d3edd5aff586edd71d9e6d01dab3bd
rajputanjali <rajputanjali7211@gmail.com>
2025-12-14 14:10:59 +0530

    question64

---
+++ question64.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    if (a == b){
+        cout << "The two numbers are equal." << endl;
+    }
+    else {
+        cout << "The two numbers are not equal." << endl;
+    }
+
+    return 0;
+
+}
