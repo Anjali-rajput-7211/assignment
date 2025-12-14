95425b9a4d181d25fa618b7b6f0cf0d256bca0ac
rajputanjali <rajputanjali7211@gmail.com>
2025-12-14 14:26:48 +0530

    question66

---
+++ question66.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float a, b, c, d;
+    cout << "enter the number:";
+    cin >> a >> b >> c >> d;
+
+    float average = (a + b + c + d) / 4;
+
+    cout << average << endl;
+
+    return 0;
+}
