b058c284e4c8b01d0e0cb18d9b00aa829d1dd59b
rajputanjali <rajputanjali7211@gmail.com>
2025-12-15 13:19:35 +0530

    question73

---
+++ question73.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    float b;
+    cout << "enter the intiger valu and float value";
+    cin >> a >> b;
+    float sum = a + b;
+    cout << sum;
+    return 0;
+}
