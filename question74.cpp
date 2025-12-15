8d7776dbcdd3980fd1a5c84d726fa1e53b50b137
rajputanjali <rajputanjali7211@gmail.com>
2025-12-15 13:36:08 +0530

    question74

---
+++ question74.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cout << "enter the string:";
+    cin >> s;
+    if(s=="a" || s=="e" || s=="i" || s=="o" || s=="u"){
+        cout << "true";
+    }
+    else
+        cout << "false";
+}
