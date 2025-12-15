00b68e20793cad1e895ef3bb5e4de4d3971dd670
rajputanjali <rajputanjali7211@gmail.com>
2025-12-15 13:13:46 +0530

    question72

--- question72.cpp
+++ question72.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 int main(){
+    int a, b;
+    cout << "enter the two number:";
+    cin >> a >> b;
 
-    
+    if(a>b){
+        cout << "true";
+    }
+    cout << "false";
     return 0;
 
 }
