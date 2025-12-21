30b77c3fc6941a18fdbfbf196f9893f35c37b135
rajputanjali <rajputanjali7211@gmail.com>
2025-12-21 15:18:52 +0530

    question83

---
+++ question83.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b, c, d, e;
+    cout << "enter the five number";
+    cin >> a >> b >> c >> d >> e;
+    int largest = 0;
+    if(b>largest)largest = b;
+    if(c>largest)largest = c;
+    if(d>largest)largest = d;
+    if(e>largest)largest = e;
+        cout <<"largest"<< largest;
+            return 0;
+}
